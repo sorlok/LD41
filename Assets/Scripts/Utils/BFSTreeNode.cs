cb94c3925949ae6b2959fbdfef75413532a2003d
lot9s <lordofthenines@gmail.com>
2018-04-21 14:52:35 -0400

    added bfs tree classes

---
+++ Assets/Scripts/Utils/BFSTreeNode.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class BFSTreeNode {
+	public Vector2Int id;
+
+	public List<int> distances;
+	public List<BFSTreeNode> children;
+
+	public BFSTreeNode(Vector2Int _id) {
+		id = _id;
+
+		distances = new List<int> ();
+		children = new List<BFSTreeNode> ();
+	}
+
+	public void AddChild (BFSTreeNode _node) {
+		children.Add (_node);
+	}
+
+	public void AddChild (Vector2Int _id) {
+		children.Add (new BFSTreeNode(_id));
+	}
+}
