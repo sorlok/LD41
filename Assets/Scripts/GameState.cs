366e20e4d6341d94d9e258b652adf0de125d4a22
Seth N. Hetu <seth.hetu@gmail.com>
2018-04-21 09:52:15 -0400

    Basic particle on button click

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -12,6 +12,12 @@ public class GameState : MonoBehaviour {
 	public Button Response2;
 	public Button Response3;
 
+	// Our "choice" particle system and response particles
+	public ParticleSystem ChoiceParticles;
+	public Material GoodOptionTexture;
+	public Material NeutralOptionTexture;
+	public Material BadOptionTexture;
+
 
 
 	private DateDialogues dateDialogues;
@@ -27,6 +33,23 @@ public class GameState : MonoBehaviour {
 	}
 
 	public void ChooseOption(int opt) {
+		// Particles
+		ChoiceParticles.transform.localPosition = new Vector3 (0, -1 * (opt - 1), 0); // Hack for now
+		if (opt == 1) {
+			ChoiceParticles.GetComponent<Renderer> ().material = GoodOptionTexture;
+		} else if (opt == 2) {
+			ChoiceParticles.GetComponent<Renderer> ().material = NeutralOptionTexture;
+		} else {
+			ChoiceParticles.GetComponent<Renderer> ().material = BadOptionTexture;
+		}
+		// TODO: Update after setting material?
+		ChoiceParticles.Play();
+
+
+		Invoke("ChoiceParticlesDone", 2);
+	}
+
+	public void ChoiceParticlesDone() {
 		// Clear boxes.
 		Response1.gameObject.SetActive (false);
 		Response2.gameObject.SetActive (false);
