cc59f894aa5fa04353447dfde6e7ede2e79a1a22
lot9s <lordofthenines@gmail.com>
2018-04-21 02:08:53 -0400

    entity - initial commit

---
+++ Assets/Scripts/Fan.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour {
+	public GameObject fan;
+
+	/* basic resources */
+	private bool aware = false;
+	private bool enraged = false;
+	private bool influenced = false;
+
+	/* movement */
+	private uint speed = 2;
+
+	private Vector3 dateLoc;
+	private Vector3 promotionLoc;
+
+	/* combat */
+	private uint damage = 1;
+
+
+	/* --- Methods --- */
+	void DoDamage () {}
+
+	bool IsPCAdjacent () {
+		return false;
+	}
+
+	void MoveToPromotion () {}
+
+	void MoveToDate () {}
+
+	void TakeTurn() {
+		if (influenced) {
+			MoveToPromotion ();
+		} else if (aware) {
+			if (IsPCAdjacent) {
+				DoDamage ();
+			} else {
+				MoveToDate ();
+			}
+		} else {
+			RandomWalk ();
+		}
+	}
+
+	void RandomWalk () {}
+
+
+	/* --- Unity Life Cycle callbacks --- */
+	// Use this for initialization
+	void Start () {}
+
+	// Update is called once per frame
+	void Update () {}
+}
