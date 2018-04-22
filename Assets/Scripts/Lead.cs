87552b958b1026f59a373728bf1c4863a9bebbf9
Seth N. Hetu <seth.hetu@gmail.com>
2018-04-21 21:59:28 -0400

    Sample Lead trackers

--- Assets/Scripts/Lead.cs
+++ Assets/Scripts/Lead.cs
@@ -8,23 +8,57 @@ public class Lead : MonoBehaviour {
 	public GameObject lead;
 	public Lead date;
 
+	// Delegate for all resources
+	public delegate void ValueChanged(uint newval);
+
+	// Helper
+	private void FireChanged(ValueChanged tracker, uint value) {
+		if (tracker != null) {
+			tracker (value);
+		}
+	}
+
 	/* basic resources */
 	private uint selfEsteem = 3;
 	public uint SelfEsteem
 	{
 		get { return selfEsteem; }
-		set { selfEsteem = value; }
+		set { selfEsteem = value; FireChanged (SelfEsteemTracker, value); }
 	}
+	public event ValueChanged SelfEsteemTracker;
 
 	private uint fanCount = 300;
+	public uint FanCount
+	{
+		get { return fanCount; }
+		set { fanCount = value; FireChanged (FanCountTracker, value); }
+	}
+	public event ValueChanged FanCountTracker;
+
 
 	/* date resources */
 	private uint connection = 50;	// range: 0-100
+	public uint Connection
+	{
+		get { return connection; }
+		set { connection = value; FireChanged (ConnectionTracker, value); }
+	}
+	public event ValueChanged ConnectionTracker;
+
+
 	private uint moves = 2;
+	public uint Moves
+	{
+		get { return moves; }
+		set { moves = value; FireChanged (MovesTracker, value); }
+	}
+	public event ValueChanged MovesTracker;
+
 
 	/* special resources */
 	private uint promotions = 3;
 	private string[] perks;
+
 
 
 
